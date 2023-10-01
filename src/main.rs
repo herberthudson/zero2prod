95d8ca820a3553220e95029f64cd60f2e6475419
Herbert Hudson <herberthudson@gmail.com>
2023-10-01 00:09:11 -0300

    feat: test and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("Listening on port: {}", port);
+    run(listener)?.await
 }
